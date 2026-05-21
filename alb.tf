ade40fcd38f53bf983c0f8512f0a0192d447411e
SebasstianPerez <sebastianperez042702@gmail.com>
2026-05-21 19:24:31 -0300

    Cicd fix, Terraform networking alb fixed

--- alb.tf
+++ alb.tf
@@ -30,12 +30,12 @@ resource "aws_lb_target_group" "AC2-tg" {
 }
 
 resource "aws_lb_listener" "AC2-lb-listener" {
-  load_balancer_arn = aws_lb.AC2-alb.id
-  port              = 88
+  load_balancer_arn = aws_lb.AC2-alb.arn
+  port              = 80
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.AC2-tg.id
+    target_group_arn = aws_lb_target_group.AC2-tg.arn
   }
 }
